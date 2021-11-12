75b3820cdbe9a8baf63847ce8666d6299acf67bf
Michelle <wuqianyi0118@gmail.com>
2021-11-12 18:21:53 -0500

    finish part 2

--- hw3/extract_training_data.py
+++ hw3/extract_training_data.py
@@ -1,33 +1,34 @@
-from conll_reader import DependencyStructure, conll_reader
-from collections import defaultdict
 import copy
 import sys
-import keras
+from collections import defaultdict
+
 import numpy as np
 
+from conll_reader import conll_reader
+
+
 class State(object):
-    def __init__(self, sentence = []):
+    def __init__(self, sentence=[]):
         self.stack = []
         self.buffer = []
-        if sentence: 
+        if sentence:
             self.buffer = list(reversed(sentence))
-        self.deps = set() 
-    
+        self.deps = set()
+
     def shift(self):
         self.stack.append(self.buffer.pop())
 
     def left_arc(self, label):
-        self.deps.add( (self.buffer[-1], self.stack.pop(),label) )
+        self.deps.add((self.buffer[-1], self.stack.pop(), label))
 
     def right_arc(self, label):
         parent = self.stack.pop()
-        self.deps.add( (parent, self.buffer.pop(), label) )
+        self.deps.add((parent, self.buffer.pop(), label))
         self.buffer.append(parent)
 
     def __repr__(self):
         return "{},{},{}".format(self.stack, self.buffer, self.deps)
 
-   
 
 def apply_sequence(seq, sentence):
     state = State(sentence)
@@ -35,121 +36,145 @@ def apply_sequence(seq, sentence):
         if rel == "shift":
             state.shift()
         elif rel == "left_arc":
-            state.left_arc(label) 
+            state.left_arc(label)
         elif rel == "right_arc":
-            state.right_arc(label) 
-         
+            state.right_arc(label)
+
     return state.deps
-   
+
+
 class RootDummy(object):
     def __init__(self):
         self.head = None
         self.id = 0
-        self.deprel = None    
+        self.deprel = None
+
     def __repr__(self):
         return "<ROOT>"
 
-     
-def get_training_instances(dep_structure):
 
+def get_training_instances(dep_structure):
     deprels = dep_structure.deprels
-    
-    sorted_nodes = [k for k,v in sorted(deprels.items())]
+
+    sorted_nodes = [k for k, v in sorted(deprels.items())]
     state = State(sorted_nodes)
     state.stack.append(0)
 
     childcount = defaultdict(int)
-    for ident,node in deprels.items():
+    for ident, node in deprels.items():
         childcount[node.head] += 1
- 
+
     seq = []
-    while state.buffer: 
+    while state.buffer:
         if not state.stack:
-            seq.append((copy.deepcopy(state),("shift",None)))
+            seq.append((copy.deepcopy(state), ("shift", None)))
             state.shift()
             continue
         if state.stack[-1] == 0:
-            stackword = RootDummy() 
+            stackword = RootDummy()
         else:
             stackword = deprels[state.stack[-1]]
         bufferword = deprels[state.buffer[-1]]
         if stackword.head == bufferword.id:
-            childcount[bufferword.id]-=1
-            seq.append((copy.deepcopy(state),("left_arc",stackword.deprel)))
+            childcount[bufferword.id] -= 1
+            seq.append((copy.deepcopy(state), ("left_arc", stackword.deprel)))
             state.left_arc(stackword.deprel)
         elif bufferword.head == stackword.id and childcount[bufferword.id] == 0:
-            childcount[stackword.id]-=1
-            seq.append((copy.deepcopy(state),("right_arc",bufferword.deprel)))
+            childcount[stackword.id] -= 1
+            seq.append((copy.deepcopy(state), ("right_arc", bufferword.deprel)))
             state.right_arc(bufferword.deprel)
-        else: 
-            seq.append((copy.deepcopy(state),("shift",None)))
+        else:
+            seq.append((copy.deepcopy(state), ("shift", None)))
             state.shift()
     return seq
 
 
-dep_relations = ['tmod', 'vmod', 'csubjpass', 'rcmod', 'ccomp', 'poss', 'parataxis', 'appos', 'dep', 'iobj', 'pobj', 'mwe', 'quantmod', 'acomp', 'number', 'csubj', 'root', 'auxpass', 'prep', 'mark', 'expl', 'cc', 'npadvmod', 'prt', 'nsubj', 'advmod', 'conj', 'advcl', 'punct', 'aux', 'pcomp', 'discourse', 'nsubjpass', 'predet', 'cop', 'possessive', 'nn', 'xcomp', 'preconj', 'num', 'amod', 'dobj', 'neg','dt','det']
+dep_relations = ['tmod', 'vmod', 'csubjpass', 'rcmod', 'ccomp', 'poss', 'parataxis', 'appos', 'dep', 'iobj', 'pobj',
+                 'mwe', 'quantmod', 'acomp', 'number', 'csubj', 'root', 'auxpass', 'prep', 'mark', 'expl', 'cc',
+                 'npadvmod', 'prt', 'nsubj', 'advmod', 'conj', 'advcl', 'punct', 'aux', 'pcomp', 'discourse',
+                 'nsubjpass', 'predet', 'cop', 'possessive', 'nn', 'xcomp', 'preconj', 'num', 'amod', 'dobj', 'neg',
+                 'dt', 'det']
 
 
 class FeatureExtractor(object):
-       
+
     def __init__(self, word_vocab_file, pos_vocab_file):
-        self.word_vocab = self.read_vocab(word_vocab_file)        
-        self.pos_vocab = self.read_vocab(pos_vocab_file)        
+        self.word_vocab = self.read_vocab(word_vocab_file)
+        self.pos_vocab = self.read_vocab(pos_vocab_file)
         self.output_labels = self.make_output_labels()
 
     def make_output_labels(self):
         labels = []
-        labels.append(('shift',None))
-    
-        for rel in dep_relations:
-            labels.append(("left_arc",rel))
-            labels.append(("right_arc",rel))
-        return dict((label, index) for (index,label) in enumerate(labels))
+        labels.append(('shift', None))
 
-    def read_vocab(self,vocab_file):
+        for rel in dep_relations:
+            labels.append(("left_arc", rel))
+            labels.append(("right_arc", rel))
+        return dict((label, index) for (index, label) in enumerate(labels))
+
+    def read_vocab(self, vocab_file):
         vocab = {}
-        for line in vocab_file: 
+        for line in vocab_file:
             word, index_s = line.strip().split()
             index = int(index_s)
             vocab[word] = index
-        return vocab     
+        return vocab
 
     def get_input_representation(self, words, pos, state):
         # TODO: Write this method for Part 2
+        result = np.array([])
         stack = state.stack
         buffer = state.buffer
-        input = np.array([])
-        if len(stack) >= 3:
-            np.append(input, [words[stack[-1]], words[stack[-2]], words[stack[-3]]])
-        if len(buffer) >= 3:
-            np.append(input, [words[buffer[-1]], words[buffer[-2]], words[buffer[-3]]])
+        for arr in [stack, buffer]:
+            for i in range(1, 4):
+                if len(arr) < i:
+                    word = '<NULL>'
+                elif arr[-i] == 0:
+                    word = '<ROOT>'
+                else:
+                    word = words[arr[-i]].lower()
+                    pos_tag = pos[arr[-i]]
+                    if pos_tag in ['CD', 'NNP']:
+                        word = '<%s>' % pos_tag
+                    elif word not in self.word_vocab:
+                        word = '<UNK>'
+                result = np.append(result, [self.word_vocab[word]])
 
-        return input
+        return result
 
-
-    def get_output_representation(self, output_pair):  
+    def get_output_representation(self, output_pair):
         # TODO: Write this method for Part 2
-        return np.zeros(91)
+        n = len(dep_relations)
+        one_hot = np.zeros(n * 2 + 1)
+        transition = output_pair[0]
+        label = output_pair[1]
 
-     
-    
+        if transition == "shift":
+            one_hot[90] = 1
+        elif transition == "left_arc":
+            one_hot[dep_relations.index(label)] = 1
+        elif transition == "right_arc":
+            one_hot[dep_relations.index(label) + n] = 1
+
+        return one_hot
+
+
 def get_training_matrices(extractor, in_file):
     inputs = []
     outputs = []
-    count = 0 
-    for dtree in conll_reader(in_file): 
+    count = 0
+    for dtree in conll_reader(in_file):
         words = dtree.words()
         pos = dtree.pos()
         for state, output_pair in get_training_instances(dtree):
             inputs.append(extractor.get_input_representation(words, pos, state))
             outputs.append(extractor.get_output_representation(output_pair))
-        if count%100 == 0:
+        if count % 100 == 0:
             sys.stdout.write(".")
             sys.stdout.flush()
         count += 1
     sys.stdout.write("\n")
-    return np.vstack(inputs),np.vstack(outputs)
-       
+    return np.vstack(inputs), np.vstack(outputs)
 
 
 if __name__ == "__main__":
@@ -158,20 +183,16 @@ if __name__ == "__main__":
     POS_VOCAB_FILE = 'data/pos.vocab'
 
     try:
-        word_vocab_f = open(WORD_VOCAB_FILE,'r')
-        pos_vocab_f = open(POS_VOCAB_FILE,'r') 
+        word_vocab_f = open(WORD_VOCAB_FILE, 'r')
+        pos_vocab_f = open(POS_VOCAB_FILE, 'r')
     except FileNotFoundError:
         print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
-        sys.exit(1) 
+        sys.exit(1)
 
-
-    with open(sys.argv[1],'r') as in_file:   
-
+    with open(sys.argv[1], 'r') as in_file:
         extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
         print("Starting feature extraction... (each . represents 100 sentences)")
-        inputs, outputs = get_training_matrices(extractor,in_file)
+        inputs, outputs = get_training_matrices(extractor, in_file)
         print("Writing output...")
         np.save(sys.argv[2], inputs)
         np.save(sys.argv[3], outputs)
-
-
